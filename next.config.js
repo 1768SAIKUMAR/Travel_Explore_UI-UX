8e336a8151e8a79613194c2e4469efa126588a86
1768SAIKUMAR <chintalasaikumar1768.1@gmail.com>
2023-10-15 11:38:26 +0530

    ignore errors

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images : {unoptimized: true}
-}
+  output: "export",
+  images: { unoptimized: true },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
